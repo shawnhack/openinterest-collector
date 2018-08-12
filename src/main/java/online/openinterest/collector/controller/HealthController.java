90547b70a083f9ca54f61e042c2694c5ae4fdfbc
Shawn Hack <shawnhack@gmail.com>
2018-08-12 11:58:51 -0400

    clean up

--- src/main/java/online/openinterest/collector/controller/HealthController.java
+++ src/main/java/online/openinterest/collector/controller/HealthController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HealthController {
 
-	@GetMapping("/healthcheck")
-	public ResponseEntity<String> healthCheck() {
-		return ResponseEntity.ok().body("Healthy");
-	}
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok().body("Healthy");
+    }
 
 }
